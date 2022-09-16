2d44ffe6a4e87b87f6004e95a9f6aeaf2139213f
Andrey <68823111+Corochco@users.noreply.github.com>
2022-09-16 22:12:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 Token = '5689487490:AAHjwCnLYovsV0BA7LpcS7gqz5iisVIUqm8'
 bot = telebot.TeleBot(Token)
@@ -24,8 +25,54 @@ def is_in_process_of_registration(id):
     f.close()
     return b"""
 
+#function with all markups
+def markups(number):
+    if number == 1:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("Show my titles")
+        item2 = types.KeyboardButton("Add title")
+        item3 = types.KeyboardButton("Rate")
+        item4 = types.KeyboardButton("Notes")
+        markup.add(item1, item2)
+        markup.add(item3, item4)
+    elif number == 2:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('Show users rate')
+        item2 = types.KeyboardButton('Estimate')
+        item4 = types.KeyboardButton('Back')
+        item3 = types.KeyboardButton('Change my rates')
+        markup.add(item1, item2)
+        markup.add(item3, item4)
+    elif number == 3:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('My notes')
+        item2 = types.KeyboardButton('Write notes')
+        item3 = types.KeyboardButton('Delete note')
+        item4 = types.KeyboardButton('Change note')
+        item5 = types.KeyboardButton('Back')
+        markup.add(item1, item2)
+        markup.add(item3, item4)
+        markup.add(item5)
+    return markup
+
+#function that show local users rating of titles
+def show_users_rate(id):
+    with open('rating.txt', 'r') as f:
+        line = f.readline()
+        counter = 0
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('Baсk')
+        markup.add(item1)
+        while line != "":
+            counter += 1
+            bot.send_message(int(id), line, reply_markup=markup)
+            line = f.readline()
+        if counter == 0:
+            bot.send_message(int(id), 'Oops... I aint have rate now... Sry...', reply_markup=markup)
+
 #function that add titles
 def add_title(id, text):
+    markup = markups(1)
     with open('add_title.txt', 'r') as f:
         line = f.readline()
         array = []
@@ -48,13 +95,15 @@ def add_title(id, text):
             line = f.readline()
     with open('user_titles.txt', 'w') as f:
         for i in array:
-            f.write(i)
-    bot.send_message(int(id), 'Your title was successfully added!')
+            f.write(i + '\n')
+    bot.send_message(int(id), 'Your title was successfully added!', reply_markup=markup)
 
 #function that show all user titles
 def show_titles(id):
     f = open('user_titles.txt', 'r')
+    markup = markups(1)
     line = f.readline()
+    counter = 0
     while line != '':
         id_now = ''
         for i in range(len(line)):
@@ -75,7 +124,7 @@ def show_titles(id):
             break
         line = f.readline()
     if counter == 0:
-        bot.send_message(int(id), 'U do not have any added titles.')
+        bot.send_message(int(id), 'U do not have any added titles.', reply_markup=markup)
 
 #function that write nikname to new user
 def write_nickname(id, text):
@@ -113,31 +162,27 @@ def was_user_registrated(id):
 def processing_commands(message):
     text = message.text
     id = message.chat.id
+    markup = markups(1)
     if text == '/start':
         nickname = was_user_registrated(id)
         if nickname != None:
-            bot.send_message(id, 'Hello again, ' + nickname)
+            bot.send_message(id, 'Hello again, ' + nickname, reply_markup=markup)
         else:
             bot.send_message(id, "Hello! I see u aint here! Lets start registration! Write your nickname.")
     elif text == '/help':
-        bot.send_message(message.chat.id, 'Welcome! This bot can help you make a library of anime you have watched and write/read reviews about them. If u wanna see all your titles, use command /show_my_titles. If u wanna add titles, write /add_title.')
-    elif text == '/show_my_titles':
-        show_titles(id)
-    elif text == '/add_title':
-        bot.send_message(id, 'Write name of the title, that u wanna add.')
-        with open('add_title.txt', 'r+') as f:
-            f.write(str(id) + '\n')
+        bot.send_message(message.chat.id, 'Welcome! This bot can help you make a library of anime you have watched and write/read reviews about them.', reply_markup=markup)
 
 #processing text messages
 @bot.message_handler(content_types='text')
 def processing_text(message):
     text = message.text
     id = message.chat.id
+    markup = markups(1)
     #is user in process of registration?
     registrate = 1 if was_user_registrated(id) == '' else 0
     if registrate:
         write_nickname(id, text)
-        bot.send_message(message.chat.id, 'Your nickname is saved! Enjoy using the bot!')
+        bot.send_message(message.chat.id, 'Your nickname is saved! Enjoy using the bot!', reply_markup=markup)
     #is user in process of add title?
     with open('add_title.txt', 'r') as f:
         line = f.readline()
@@ -148,5 +193,60 @@ def processing_text(message):
             line = f.readline()
     if b:
         add_title(id, message.text)
+    if text == 'Show my titles':
+        show_titles(id)
+    elif text == 'Add title':
+        bot.send_message(id, 'Write name of the title, that u wanna add.', reply_markup=None)
+        with open('add_title.txt', 'r+') as f:
+            f.write(str(id) + '\n')
+    elif text == 'Rate':
+        markup = markups(2)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    elif text == 'Show users rate':
+        show_users_rate(id)
+    elif text == 'Notes':
+        markup = markups(3)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    elif text == 'Back':
+        markup = markups(1)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    #this back from notes
+    elif text == 'Bаck':
+        markup = markups(3)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    #this back from rate
+    elif text == 'Baсk':
+        markup = markups(2)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    elif text == 'Estimate':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton('Baсk')
+        markup.add(item)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    elif text == 'Change my rates':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton('Baсk')
+        markup.add(item)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    elif text == 'My notes':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton('Bаck')
+        markup.add(item)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    elif text == 'Write notes':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton('Bаck')
+        markup.add(item)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    elif text == 'Delete note':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton('Bаck')
+        markup.add(item)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
+    elif text == 'Change note':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item = types.KeyboardButton('Bаck')
+        markup.add(item)
+        bot.send_message(int(id), '-_-', reply_markup=markup)
 
 bot.infinity_polling()
